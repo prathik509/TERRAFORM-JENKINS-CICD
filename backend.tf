9926f9ed2db1f53b01d1c44af44d21390b58f85a
prathik509 <54430815+prathik509@users.noreply.github.com>
2023-10-04 17:49:09 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "raghu-devops"  #change
     key            = "my-terraform-environment/main"
-    region         = "us-east-1"
+    region         = "us-east-1"     #change
     dynamodb_table = "mrcloudbook-dynamo-db-tables"    #change 
   }
 }
